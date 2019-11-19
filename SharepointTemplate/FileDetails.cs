f7ee1d4102583d5cccccbfcb8101836252ebfa5f
Chris Hanna <chhanna@microsoft.com>
2019-11-19 15:47:52 -0500

    Modified to Improve performance and to handle subdirectories

---
+++ SharepointTemplate/FileDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SharepointTemplate
+{
+   public class FileDetails
+    {
+       public string futurePath { get; set; }
+       public File sharepointfile { get; set; }
+    }
+}
